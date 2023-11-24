e7df594806dd1f6b2dbd96b6c15dd3476d6617dc
65456u <yanghongchen@bupt.edu.cn>
2023-11-24 21:19:59 +0800

    Update oag_dataset.py

--- openhgnn/dataset/oag_dataset.py
+++ openhgnn/dataset/oag_dataset.py
@@ -1,9 +1,9 @@
+import os
+
 import dgl
-import dgl.function as fn
 import torch as th
-import numpy as np
-from dgl.data.utils import load_graphs, save_graphs, extract_archive, download
-from . import load_acm_raw
+from dgl.data import extract_archive, download
+
 from . import BaseDataset, register_dataset
 
 
@@ -11,14 +11,19 @@ from . import BaseDataset, register_dataset
 class OAGDataset(BaseDataset):
     def __init__(self, *args, **kwargs):
         super(OAGDataset, self).__init__(*args, **kwargs)
+        self.name = "oag_cs"
+        self.valid_idx = None
+        self.test_idx = None
+        self.train_idx = None
+        self.dims = None
         self.g = None
         self.category = None
         self.num_classes = None
         self.has_feature = True
         self.data_path = "./openhgnn/dataset/data/oag_cs.tgz"
-        self.raw_dir = "./openhgnn/data"
+        self.raw_dir = "./openhgnn/dataset/data"
         self.g_path = "./openhgnn/dataset/data/oag_cs/oag_cs.bin"
-        url = "https://s3.cn-north-1.amazonaws.com.cn/dgl-data/dataset/oag_cs.tgz"
+        self.url = "https://s3.cn-north-1.amazonaws.com.cn/dgl-data/dataset/oag_cs.tgz"
         if not self.has_cache():
             self.download()
         self.load_graph_from_disk(self.g_path)
@@ -56,7 +61,7 @@ class OAGDataset(BaseDataset):
         return self.train_idx, self.valid_idx, self.test_idx
 
     def get_feature(
-        self,
+            self,
     ):
         return self.g.ndata.pop("feat")
 
@@ -69,8 +74,9 @@ class OAGDataset(BaseDataset):
             pass
         else:
             file_path = os.path.join(self.raw_dir)
+            print("Downloading", self.url, "to", file_path)
             download(self.url, path=file_path)
-        extract_archive(self.data_path, os.path.join(self.raw_dir, self.name))
+        extract_archive(self.data_path, os.path.join(self.raw_dir))
 
     def has_cache(self):
         return os.path.exists(self.g_path)
